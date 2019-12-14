7e28e28933a6b4a774479f3a1eb3737a5db89b1e
Jeronimo <jeronimomora@berkeley.edu>
2019-12-14 13:54:15 -0800

    Update alert template with proper overriding

--- src/components/StyledAlertTemplate.js
+++ src/components/StyledAlertTemplate.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import AlertTemplate from 'react-alert-template-basic-with-icons'
 
-const alertStyle = {
+const styleOverrides = {
     backgroundColor: 'white',
     color: 'black',
-    padding: '10px',
-    textTransform: 'uppercase',
-    borderRadius: '3px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    boxShadow: '0px 2px 2px 2px rgba(0, 0, 0, 0.03)',
-    fontFamily: 'Arial',
-    width: '400px',
-    boxSizing: 'border-box'
+    width: '450px'
 }
 
 export default (props) => (
-    <AlertTemplate 
+    <AlertTemplate
+        {...props}
         iconColors={{ close: 'black' }} 
-        alertStyle={alertStyle}
-        {...props} 
+        style={{ ...props.style, ...styleOverrides }}
     />
 )
